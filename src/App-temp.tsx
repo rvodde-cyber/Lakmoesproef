4a0c2190f4fc7b7cea2b554b6865bb5d137f9b9c
rvodde-cyber <r.vodde@gmail.com>
2026-04-01 11:45:29 +0200

    fix: temporarily rename App.tsx to App-temp.tsx to fix case sensitivity

---
+++ src/App-temp.tsx
@@ -0,0 +1,8 @@
+// Temporarily renamed file. This will be eventually renamed back.
+import React from 'react';
+
+const App = () => {
+    return <div>Hello, World!</div>;
+};
+
+export default App;
