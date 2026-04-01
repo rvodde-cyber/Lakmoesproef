3c1d4818bc718148b23f62f62f764c28fb941a21
rvodde-cyber <r.vodde@gmail.com>
2026-04-01 11:51:05 +0200

    Delete src/App-temp.tsx

--- src/App-temp.tsx
+++
@@ -1,8 +0,0 @@
-// Temporarily renamed file. This will be eventually renamed back.
-import React from 'react';
-
-const App = () => {
-    return <div>Hello, World!</div>;
-};
-
-export default App;
